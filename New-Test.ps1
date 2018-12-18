0c347afa1c62e9c1e21e0b785f16a776243ae2de
Logan Rakai <lmrakai@gmail.com>
2018-12-17 23:50:08 -0700

    Test logic

--- New-Test.ps1
+++ New-Test.ps1
@@ -3,11 +3,24 @@ function New-Test {
         $Parameters
     )
 
-}
+    if (-Not (Parameters['managedDisks']) -And (Parameters['DiskType'] -eq "StandardSSD_LRS")) {
+        Write-Error "Standard SSDs and Unmanaged disks are not supported in Azure"
+        Exit
+    }
+    
+    #Create or check for existing resource group
+    $resourceGroup = Get-AzureRmResourceGroup -Name $Parameters['resourceGroupName'] -ErrorAction SilentlyContinue
+    if (!$resourceGroup) {
+        Write-Host "Creating resource group '$resourceGroupName' in location " $Parameters['location'];
+        New-AzureRmResourceGroup -Name $resourceGroupName -Location $Parameters['location']
+    }
+    else {
+        Write-Host "Using existing resource group '$resourceGroupName'";
+    }
 
-if (-Not (Parameters['managedDisks']) -And (Parameters['DiskType'] -eq "StandardSSD_LRS") {
-    Write-Error "Standard SSDs and Unmanaged disks are not supported in Azure"
-   Exit
+    New-AzureRmResourceGroupDeployment `
+        -ResourceGroupName $Parameters['resourceGroupName'] `
+        -TemplateFile .\infra\arm-template.json `
+        -TemplateParameterFile .\infra\commonParameters.json `
+        -TemplateParameterObject $Parameters
 }
-
-New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
